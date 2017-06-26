b5025aaa5e0f0236d3fd302748ca7d49e34c9c90
Spencer Wood <spencercwood@gmail.com>
2017-06-25 22:55:39 -0600

    Install node 8

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y git htop
 git config --global user.name scwood
 git config --global user.email spencercwood@gmail.com
 
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 
