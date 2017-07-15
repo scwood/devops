894cc66ef020d464482252b53eaeaf1a44068fcb
scwood <spencercwood@gmail.com>
2017-07-15 03:52:18 +0000

    Add nginx to install script, point tetris to s3

--- install.sh
+++ install.sh
@@ -8,8 +8,7 @@ git config --global user.name scwood
 git config --global user.email spencercwood@gmail.com
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get install -y build-essential
+sudo apt-get install -y nodejs build-essential nginx
 
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
